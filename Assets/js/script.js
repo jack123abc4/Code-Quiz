5b8532ec7e7fcf2ee00d38db417954bde4eb8ec6
jack123abc4 <jack.r.motherway@gmail.com>
2022-08-22 16:03:18 -0400

    start menu, first question displayed. fixed timer display issue

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,28 +1,65 @@
-timerEl = document.querySelector("#timer");
+var timerEl = document.querySelector("#timer");
+var mainTextEl = document.querySelector("#main-text");
+var startButton = document.createElement("button");
+var secondsLeft = 0;
+
+var questions = ["What does DOM stand for?", "Which of the following is not considered a primitive type in Javascript?", "Which comparison symbol is used to check for strict equality?"];
+var answerOptions = [["Dramatic Overreaction, Mom", "Document Object Model", "Data-Oriented Mechanism", "Dentists' Organization of Mouths"], ["String", "Boolean", "Number", "Object"], ["==", "!=", "===", "!=="]];
+var correctAnswers = ["Document Object Model", "Object", "==="];
 
 
-
-var secondsLeft = 10;
 function setTime() {
+    secondsLeft = 10;
+    timerEl.children[0].textContent = secondsLeft;
     var timerInterval = setInterval(
         function() {
             secondsLeft--;
             timerEl.children[0].textContent = secondsLeft;
             if (secondsLeft === 0){
                 clearInterval(timerInterval);
-                alert("Time's up!");
+                // alert("Time's up!");
             }
         },
         1000
     )
 }
 
+function displayQuestion(qNum) {
+    mainTextHeaderEl.textContent = questions[qNum];
+    // for (var i = 0; i < answerOptions[qNum].length; i++) {
+    //     var answerButton = document.createElement("button");
+    //     answerButton.textContent
+    // }
+    mainTextParagraphEl.textContent = answerOptions[qNum];
+}
 
+function startMenu() {
+    mainTextHeaderEl = mainTextEl.children[0];
+    mainTextParagraphEl = mainTextEl.children[1];
 
+    mainTextHeaderEl.textContent = "Coding Quiz Challenge";
+    mainTextParagraphEl.textContent = "Test your skills in this coding quiz challenge! Answer correctly and beat the timer to set a new highscore. But remember - wrong answers will cost you!";
+
+    startButton.style.display = "block";
+    startButton.classList.add("center");
+    startButton.textContent = "Start Game";
+    mainTextEl.appendChild(startButton);
+}
+
+function startGame() {
+    console.log("Start Game!");
+    setTime();
+    startButton.style.display = "none";
+    displayQuestion(0);
+}
+
+startButton.addEventListener("click", function() {
+    startGame()
+})
 
 function init() {
     timer.children[0].textContent = secondsLeft;
-    setTime();
+    startMenu();
 }
 
 
